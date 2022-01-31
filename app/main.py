9c30d4307f1ee4d57e7efeb90618f3abe6462196
Derien Stephens <derien.stephens@Deriens-MBP.lan>
2022-01-30 19:33:51 -0800

    Add  user registration via login radius

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import requests
 
 app = FastAPI()
 
-
 @app.get("/")
-async def index():
-    return {"Hello World"}
+def index():
+    content = """
+    <html>
+            <a href="https://career-stack.hub.loginradius.com/auth.aspx?return_url=https://career-stack.herokuapp.com"><h4>Login</h4></a>
+    </html>
+    """
+
+    return HTMLResponse(content=content, status_code=200)
+
