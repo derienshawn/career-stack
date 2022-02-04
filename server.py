a0a29792af02b026ee28df2d458ecebbef2a653b
Derien Stephens <derien.stephens@Deriens-MBP.lan>
2022-02-04 08:34:55 -0800

    Replace main.py with server.py

--- server.py
+++ server.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from app.routes.user import user
 
 
 app = FastAPI()
 app.include_router(user)
+
+
+@app.get("/")
+def index():
+    content = """
+    <html>
+            <a href="https://career-stack.hub.loginradius.com/auth.aspx?return_url=https://career-stack.herokuapp.com"><h4>Login</h4></a>
+    </html>
+    """
+    return HTMLResponse(content=content, status_code=200)     
