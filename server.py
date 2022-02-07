22745e5512e2d93e4deac45a28de724e2c4a7a14
Derien Stephens <derien.stephens@Deriens-MBP.lan>
2022-02-07 07:11:09 -0800

    Commit server.py

--- server.py
+++ server.py
@@ -15,22 +15,13 @@ import os
 redis_url = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 redis = redis.from_url(redis_url)
 
-# LR.API_KEY = "5a253b16-8b8e-49da-8bd6-5fcf6ad5a968"
-# LR.API_SECRET = "5a253b16-8b8e-49da-8bd6-5fcf6ad5a968"
-
 app = FastAPI()
-# loginradius = LR()
 app.include_router(user, tags=["user"])
 app.include_router(project, tags=["project"])
-# session = requests.Session()
-
-# register_local_url = "https://career-stack.hub.loginradius.com/auth.aspx?action=register&return_url=http://127.0.0.1:8000/register-redirect"
-# register_staging_url = "https://career-stack.hub.loginradius.com/auth.aspx?action=register&return_url=https://career-stack.herokuapp.com/register-redirect"
 
 
 @app.get("/register")
 def register():
-
    return login_radius.register()
 
 
