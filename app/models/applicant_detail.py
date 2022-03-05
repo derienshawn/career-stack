386076d93457f3bad3872c945c9357c81f34e31c
Derien Stephens <derien.stephens@Deriens-MBP.lan>
2022-03-05 05:37:44 -0800

    Update applicant_detail model to support email

--- app/models/applicant_detail.py
+++ app/models/applicant_detail.py
@@ -4,6 +4,7 @@ from datetime import datetime
 class ApplicantDetail(BaseModel):
     user_id: str
     project_id: str
+    email: str
     role_of_interest: str
     applied_at: datetime
     application_status: str
--- app/schemas/applicant_detail.py
+++ app/schemas/applicant_detail.py
@@ -3,6 +3,7 @@ def applicantDetailEntity(item) -> dict:
         "_id":str(item["_id"]),
         "user_id": item["user_id"],
         "project_id": item["project_id"],
+        "email": item["email"],
         "role_of_interest": item["role_of_interest"],
         "applied_at": item["applied_at"],
         "application_status": item["application_status"],
